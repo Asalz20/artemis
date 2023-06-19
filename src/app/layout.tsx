546c59ea27a9859d12205a361aafc1349ba82a9a
Alex Salazar <helloworld3197@gmail.com>
2023-06-19 16:18:29 -0400

    Set up global font and base styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 export const metadata = {
   title: "Artemis",
@@ -6,14 +8,24 @@ export const metadata = {
     "An alternative to the popular Reddit app Apollo, built with Next.js and TypeScript.",
 };
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {children}
+      </body>
     </html>
   );
 }
